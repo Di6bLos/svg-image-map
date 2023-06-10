345259064f10ba5dcef90d9105cbd8f266771fcc
CarlosPrieto <carlosprieto66@icloud.com>
2023-06-10 12:27:56 -0700

    script keeps circle on 'hover' effect when entering the text

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// Get all the .map-tag elements
+const mapTags = document.querySelectorAll(".map-tag");
 
-const mapTags = document.querySelectorAll('.map-tag');
+// Loop through each .map-tag element
+mapTags.forEach((mapTag) => {
+  // Get the circle element inside the current .map-tag element
+  const circle = mapTag.querySelector("circle");
 
+  // Add event listener for mouseenter (cursor entering the .map-tag element)
+  mapTag.addEventListener("mouseenter", () => {
+    // Add a CSS class to toggle the fill color
+    circle.classList.add("hovered");
+  });
+
+  // Add event listener for mouseleave (cursor leaving the .map-tag element)
+  mapTag.addEventListener("mouseleave", () => {
+    // Remove the CSS class to revert the fill color
+    circle.classList.remove("hovered");
+  });
+});
